f1757fb1607b63a16c0aa60dd2699ab9b20343c4
Huy Pham <phamhm@gmail.com>
2012-04-22 18:17:04 -0700

    finish project 13

--- problem_13/main.cpp
+++ problem_13/main.cpp
@@ -2,17 +2,31 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
-
+#include <cstdlib>
 int main()
 {
 	using namespace std;
 
-	vector<string> vStr;
+	vector<int> array(50,0);
 	string sTmp;
 	ifstream inFile("data.txt");
-	
-	while ( getline(inFile,sTmp))
-		vStr.push_back(sTmp);
+	int index;
 
+	// extract 23293029183023 into an array
+	while ( getline(inFile,sTmp))
+		{
+			index=0;
+			for ( string::reverse_iterator it=sTmp.rbegin() ; it < sTmp.rend() ; ++it)
+				{
+					array[index++] += (int(*it))-48;
+				}
+		}
+	for ( int i = 0 ; i < 49 ; ++i)
+		{
+			array[i+1] += (array[i]/10);
+			array[i] %= 10;
+		}
+	for ( int i = 49  ; i >0  ; --i)
+		cout << array[i];
 	return 0;
 }
